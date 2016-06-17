1ba0d8fb4ad5805901f666832580a437201c2300
Ev Bogue <ev@evbogue.com>
2016-06-17 15:19:48 -0500

    minor tweaks

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var pull = require('pull-stream')
-  , ssbclient = require('ssb-client')
+  , ssbClient = require('ssb-client')
   , chalk = require('chalk');
 
-var sbot_cont = function(cb) {
-    ssbclient(function(err, sbot) {
+var sbot = function(cb) {
+    ssbClient(function(err, sbot) {
         if (err) throw err;
         cb(sbot);
     });
 };
 
-sbot_cont(function(sbot) {
+sbot(function(sbot) {
      pull(
           sbot.messagesByType({ type: 'micro', live: true }),
           pull.drain(function (msg) { console.log (
