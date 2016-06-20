23d13f72204ad6ad32ff75acdaa713ca1ff7bc71
Ev Bogue <ev@evbogue.com>
2016-06-20 17:40:52 -0500

    remove unnecessary sbot call and move id to left

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ var pull = require('pull-stream'),
     moment = require('moment'),
     avatar = require('ssb-avatar');
 
-var sbot = function(cb) {
-    ssbClient(function(err, sbot) {
-        if (err) throw err;
-        cb(sbot);
-    });
-};
-
 ssbClient(function(err, sbot) {
     if (err) throw err;
     pull(
@@ -20,9 +13,9 @@ ssbClient(function(err, sbot) {
             var time = msg.value.timestamp;
             var cont = msg.value.content.text;
             console.log(
-                cont +
-                " " +
                 chalk.cyan(id) +
+                " " +
+                cont +
                 " " +
                 chalk.dim(moment(time).fromNow())
             )
