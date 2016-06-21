67808ffef8527edbdb0b519accfd480253fcf0c3
Ev Bogue <ev@evbogue.com>
2016-06-21 11:14:30 -0500

    make some progress

--- getavatar.js
+++ getavatar.js
@@ -1,34 +1,39 @@
-// This example doesn't work, returns sbot.links is not a function
-
 var pull = require('pull-stream'),
+    paramap = require('pull-paramap'),
     ssbClient = require('ssb-client'),
     chalk = require('chalk'),
     moment = require('moment'),
     avatar = require('ssb-avatar');
+       
+var listMessages = function() {
+    ssbClient(function(err, sbot) {
+        if (err) throw err;
+        sbot.whoami( function (err, me) {
+            pull(sbot.messagesByType({
+                type: process.argv[2] || 'micro',
+                live: true
+            }), 
+            paramap(function (sbot, msg, cb) {
+                avatar(sbot, me.id, msg.value.author, function (err, avatar) {
+                    msg.avatar = avatar;
+                    cb(null, msg);
+                });
+            }),
+            pull.drain(function(msg) {
+                var userid = msg.value.author;
+                var time = msg.value.timestamp;
+                var cont = msg.value.content.text;
+                console.log(
+                    chalk.cyan(avatar(msg.value.author)) + 
+                    " " + 
+                    cont +
+                    " " + 
+                    chalk.dim(moment(time).fromNow())
+                );
+            }));
+        });
+   });
+};
 
-ssbClient(function(err, sbot) {
-    if (err) throw err;
-    pull(
-        sbot.messagesByType({ type: process.argv[2] || 'micro', live: true }),
-        sbot.whoami(function (err, me, userid) {
-            avatar(sbot, me.id, userid || me.id, function (err, data) {
-                   console.log(JSON.stringify(data, null, 2))
-                   return data;
-                   sbot.close()
-            })
-        }),
-        pull.drain(function(msg) {
-            var userid = msg.value.author;
-            var time = msg.value.timestamp;
-            var cont = msg.value.content.text;
-            console.log(avatar(userid))
-            console.log(
-                chalk.cyan(userid) +
-                " " +
-                cont +
-                " " +
-                chalk.dim(moment(time).fromNow())
-            )
-        })
-    )
-});
+listMessages()
+
