8375835f7b48efadb18829445e770c3e8a308776
Ev Bogue <ev@evbogue.com>
2016-06-27 21:24:51 -0500

    move working code to index

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 var pull = require('pull-stream'),
+    paramap = require('pull-paramap'),
     ssbClient = require('ssb-client'),
     chalk = require('chalk'),
     moment = require('moment'),
     avatar = require('ssb-avatar');
+       
+var listMessages = function() {
+    ssbClient(function(err, sbot) {
+        if (err) throw err;
+        sbot.whoami( function (err, me) {
+            pull(sbot.messagesByType({
+                type: process.argv[2] || 'micro',
+                live: true
+            }), 
+            paramap(function(msg, cb) {
+                avatar(sbot, me.id, msg.value.author, function (err, avatar) {
+                    msg.avatar = avatar;
+                    cb(null, msg);
+               })
+            }),
+            pull.drain(function(msg) {
+                console.log(
+                    chalk.cyan(msg.avatar.name) + 
+                    " " + 
+                    msg.value.content.text +
+                    " " + 
+                    chalk.dim(moment(msg.value.timestamp).fromNow())
+                );
+            }));
+        });
+   });
+};
 
-ssbClient(function(err, sbot) {
-    if (err) throw err;
-    pull(
-        sbot.messagesByType({ type: process.argv[2] || 'micro', live: true }),
-        pull.drain(function(msg) {
-            var id = msg.value.author;
-            var time = msg.value.timestamp;
-            var cont = msg.value.content.text;
-            console.log(
-                chalk.cyan(id) +
-                " " +
-                cont +
-                " " +
-                chalk.dim(moment(time).fromNow())
-            )
-        })
-    )
-});
+listMessages()
+
